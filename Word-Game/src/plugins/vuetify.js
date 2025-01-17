e3e7205532ac76b3c2bf37714114ead6b109e89a
Malinjoh <malin.johansson@iths.se>
2025-01-17 15:25:28 +0100

    lagt till i vuetify

--- Word-Game/src/plugins/vuetify.js
+++ Word-Game/src/plugins/vuetify.js
@@ -18,4 +18,16 @@ import * as directives from 'vuetify/directives';
 export default createVuetify({
   components,
   directives,
-})
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+        },
+      },
+    },
+  },
+});
