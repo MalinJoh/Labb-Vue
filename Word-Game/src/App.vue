ec6abb3ebce690733a9d5b2ab25705934645c40e
Malinjoh <malin.johansson@iths.se>
2025-01-17 15:30:10 +0100

    fixat navbaren

--- Word-Game/src/App.vue
+++ Word-Game/src/App.vue
@@ -1,22 +1,25 @@
 <script setup>
-  import NavBar from './components/NavBar.vue'
+import NavBar from './components/NavBar.vue';
+import { useDisplay } from 'vuetify'; // Import Vuetify composable
+
+const { xsOnly } = useDisplay(); // Access breakpoint
 </script>
+
 <template>
   <v-app>
-    <!-- Sidomeny med navigationslänkar -->
-    <v-navigation-drawer app :temporary="$vuetify.breakpoint.xsOnly" permanent>
+    <!-- Responsive Navigation Drawer -->
+    <v-navigation-drawer app :temporary="xsOnly" permanent>
       <NavBar />
     </v-navigation-drawer>
     <v-main>
-      <v-container class="fill-height" fluid >
-        <v-row align:center
-        justify="center">
-        <v-col cols="12" sm="20" md="20">
-          <!-- router länkar -->
-      <router-view />
-    </v-col>
-    </v-row>
-    </v-container>
+      <v-container class="fill-height" fluid>
+        <v-row align:center justify="center">
+          <v-col cols="12" sm="20" md="20">
+            <!-- Router View -->
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
