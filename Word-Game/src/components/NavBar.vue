9ca6a6ae594df2686ddc2d44b62ef46831297699
Malinjoh <malin.johansson@iths.se>
2025-01-19 13:45:13 +0100

    tagit bort onödig kod

--- Word-Game/src/components/NavBar.vue
+++ Word-Game/src/components/NavBar.vue
@@ -1,8 +1,5 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-import { useDisplay } from 'vuetify';
-
-const { xsOnly } = useDisplay();
 </script>
 
 <template>
@@ -11,36 +8,16 @@ const { xsOnly } = useDisplay();
     <v-toolbar-title>Your Definition Page</v-toolbar-title>
     <v-spacer></v-spacer>
 
-    <!-- Links for Desktop -->
-    <template v-if="!xsOnly">
-      <v-btn text>
-        <RouterLink to="/" style="color: inherit;">Definition</RouterLink>
-      </v-btn>
-      <v-btn text>
-        <RouterLink to="/Game" style="color: inherit;">The Game</RouterLink>
-      </v-btn>
+        <!-- Links for Desktop -->
+        <v-btn text>
+          <RouterLink to="/" style="color: inherit;">Definition</RouterLink>
+        </v-btn>
+        <v-btn text>
+          <RouterLink to="/Game" style="color: inherit;">The Game</RouterLink>
+        </v-btn>
+      </v-app-bar>
     </template>
 
-    <!-- Hamburger Menu for Mobile -->
-    <template v-else>
-      <v-btn icon>
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </template>
-  </v-app-bar>
-
-  <!-- Mobile Navigation Drawer -->
-  <v-navigation-drawer v-if="xsOnly" v-model="drawer" app temporary>
-    <v-list nav dense>
-      <v-list-item>
-        <RouterLink to="/" style="color: inherit;">Definition</RouterLink>
-      </v-list-item>
-      <v-list-item>
-        <RouterLink to="/Game" style="color: inherit;">The Game</RouterLink>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
-</template>
 
 <style scoped>
 /* Ensure the app bar doesn't take excessive space */
